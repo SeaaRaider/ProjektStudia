93c7ee7c1c5d10cebd14c7ddb42d8d88452566b6
Mateusz <m4.jankowski@gmail.com>
2022-10-22 20:17:31 +0200

    Dodano funkcje InsertLoop

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 #include <iostream>
+
+using namespace std;
+
+float InsertLoop(string kwota)
+{
+    cout << "Wprowadz kwote potwierdzajac ENTER \n";
+    while(true)
+    {
+        cin >> kwota;
+        //getline(cin, kwota);
+        if(kwota.empty())
+        {
+            break;
+        }
+        else if(kwota.empty() == false)
+        {
+            return stof(kwota);
+        }
+        else
+        {
+            cout << "Error \n";
+        }
+    }
+}
+
+int main()
+{
+    float wynagrodzenia[] = {};
+
+
+    return 0;
+}
